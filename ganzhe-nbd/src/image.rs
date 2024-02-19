abd64e5b562c875234d0eb856a218bbbdc9c64bf
Chen Yuanrun <chen-yuanrun@foxmail.com>
2024-02-19 23:02:15 +0800

    feat: add MemImage implementation
    Signed-off-by: Chen Yuanrun <chen-yuanrun@foxmail.com>

--- ganzhe-nbd/src/image.rs
+++ ganzhe-nbd/src/image.rs
@@ -1,5 +1,10 @@
 #![allow(dead_code)]
 
+use std::{
+    cell::{Ref, RefCell},
+    rc::Rc,
+};
+
 use async_trait::async_trait;
 
 type IoResult<T> = std::io::Result<T>;
@@ -8,7 +13,7 @@ type IoResult<T> = std::io::Result<T>;
 pub trait Image {
     fn stat(&self) -> ImageStat;
     async fn read(&self, offset: usize, buf: &mut [u8]) -> IoResult<usize>;
-    async fn write(&self, offset: usize, buf: [u8]) -> IoResult<usize>;
+    async fn write(&self, offset: usize, buf: &[u8]) -> IoResult<usize>;
     async fn write_zero(&self, offset: usize, length: usize) -> IoResult<usize>;
     async fn flush(&self) -> IoResult<()>;
     async fn trim(&self, offset: usize, length: usize) -> IoResult<()>;
@@ -20,3 +25,92 @@ pub struct ImageStat {
 }
 
 pub trait ImageProvider {}
+
+#[derive(Clone)]
+pub struct MemImage {
+    inner: Rc<RefCell<MemImageInner>>,
+}
+
+impl MemImage {
+    pub fn new(size: usize) -> IoResult<Self> {
+        let mut buf = Vec::with_capacity(size);
+        unsafe { buf.set_len(size) };
+        Ok(Self {
+            inner: Rc::new(RefCell::new(MemImageInner { size, buf })),
+        })
+    }
+}
+
+struct MemImageInner {
+    size: usize,
+    buf: Vec<u8>,
+}
+
+#[async_trait(?Send)]
+impl Image for MemImage {
+    fn stat(&self) -> ImageStat {
+        ImageStat {
+            size: self.inner.borrow().size,
+        }
+    }
+
+    async fn read(&self, offset: usize, buf: &mut [u8]) -> IoResult<usize> {
+        let mut length = buf.len();
+        let inner = self.inner.borrow_mut();
+        if offset + length > inner.size {
+            length = inner.size - offset;
+        }
+        unsafe {
+            std::ptr::copy_nonoverlapping(
+                inner.buf[offset..offset + length].as_ptr(),
+                buf.as_mut_ptr(),
+                length,
+            );
+        }
+        Ok(length)
+    }
+
+    async fn write(&self, offset: usize, buf: &[u8]) -> IoResult<usize> {
+        let mut length = buf.len();
+        let mut inner = self.inner.borrow_mut();
+        if offset + length > inner.size {
+            length = inner.size - offset;
+        }
+        unsafe {
+            std::ptr::copy_nonoverlapping(
+                buf.as_ptr(),
+                (&mut inner.buf[offset..offset + length]).as_mut_ptr(),
+                length,
+            );
+        }
+        Ok(length)
+    }
+
+    async fn write_zero(&self, offset: usize, mut length: usize) -> IoResult<usize> {
+        let mut inner = self.inner.borrow_mut();
+        if offset + length > inner.size {
+            length = inner.size - offset;
+        }
+        unsafe {
+            std::ptr::write_bytes(
+                (&mut inner.buf[offset..offset + length]).as_mut_ptr(),
+                0,
+                length,
+            );
+        }
+        Ok(length)
+    }
+
+    async fn flush(&self) -> IoResult<()> {
+        Ok(())
+    }
+
+    async fn trim(&self, offset: usize, length: usize) -> IoResult<()> {
+        self.write_zero(offset, length).await?;
+        Ok(())
+    }
+
+    fn dup(&self) -> Box<dyn Image> {
+        Box::new(self.clone())
+    }
+}
